299173781b3ff471841b32eb6070ddb345130211
johngaitho05 <johngaitho05@gmail.com>
2020-04-10 16:05:53 +0300

    Intro to jwt

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,13 @@
-from django.conf.urls.static import static
-from django.contrib import admin
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from django.urls import path, include
 from .views import login_view
 
 urlpatterns = [
-    path('login', login_view),
+    path('login/', login_view),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
